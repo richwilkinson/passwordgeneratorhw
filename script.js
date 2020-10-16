21047e1b06332bb86a8ee9a4fd9000b926c26a68
rich wilk <richardwilkinson@live.com>
2020-10-16 13:01:02 -0400

    added onclick event to button, and a function for generating and displaying password to js file

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 //generate random password
-function create(){
+function generate(){
     //set password length/diversity
     let complexity = document.getElementById("slider").value;
     //possible password characters
-    let values = "ABCDEFGHIJKLMNOPQRSTUVWYXZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_+"
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWYXZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_+";
 
     let password = "";
 
     //for loop to choose password character
     for(var i = 0; i <= complexity; i++){
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(value.legth - 1)));
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
     }
 
+    //displays password in textbox
+    document.getElementById("display").value = password;
 
 }
+console.log(generate)
