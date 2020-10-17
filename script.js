01d36fcadeddd1d164642e4107d32fb40514a9f0
rich wilk <richardwilkinson@live.com>
2020-10-16 21:25:51 -0400

    added input options for lowercase, uppercase, numbers and symbols. also created functions that worked to their respective categories. added more responsive text.

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
+//Generator functions
+
+function getRandomLow() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+console.log(getRandomLow());
+
+function getRandomUp() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+console.log(getRandomUp());
+
+function getRandomNum() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+console.log(getRandomNum());
+
+function getRandomSym() {
+    const symbols = "!@#$%^&*()_+";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
+console.log(getRandomSym());
 //generate random password
 function generate(){
     //set password length/diversity
     let complexity = document.getElementById("slider").value;
+    console.log("complexity")
     //possible password characters
     let values = "ABCDEFGHIJKLMNOPQRSTUVWYXZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_+";
-
+    console.log("values")
     let password = "";
 
     //for loop to choose password character
@@ -20,6 +43,22 @@ function generate(){
 
 }
 
-//set default length display of 8
-document.getElementById("length").innerHTML = "length: 8"
-console.log(generate)
+//set default length display of 68
+document.getElementById("length").innerHTML = "length: 68"
+
+//function to set length in slider
+document.getElementById("slider").oninput = function(){
+    if( document.getElementById("slider").value > 0){
+        document.getElementById("length").innerHTML = "length: " + document.getElementById("slider").value;
+    }
+    else{
+        document.getElementById("length").innerHTML = "length: 1"
+    }
+}
+
+//function to copy pass in clipboard
+function copyPass(){
+    document.getElementById("display").select();
+    document.execCommand("copy");
+    alert("Password copied!");
+}
