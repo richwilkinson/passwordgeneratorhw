9c21473f1bf8dac265a8197f15afd87c77294fe0
rich wilk <richardwilkinson@live.com>
2020-10-18 15:47:40 -0400

    fixed copy button and added comments to all files

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//values of different variables
 let characterAmountRange = document.getElementById("characterAmountRange")
 
 let characterAmountNumber = document.getElementById("characterAmountNumber")
@@ -20,7 +21,7 @@ let number_char_code = arrayLowHigh(48, 57)
 
 let symbol_char_code = arrayLowHigh(33, 47).concat(arrayLowHigh(58, 64)
 )
-
+//event listeners for button/checkboxes
 characterAmountNumber.addEventListener("input", syncCharacterAmount)
 characterAmountRange.addEventListener("input", syncCharacterAmount) 
 
@@ -32,16 +33,17 @@ form.addEventListener("submit", e => {
     let includeSymbol = includeSymbolElement.checked
     let password = generatePassword(characterAmount, includeUppercase,
         includeNumber, includeSymbol)
-        display.innerText = password
+        display.value = password
     })
-    
+    //function that generates password
     function generatePassword(characterAmount, includeUppercase,
         includeNumber, includeSymbol) {
             let charCodes = lowercase_char_code
             if (includeUppercase) charCodes = charCodes.concat(uppercase_char_code)
             if (includeNumber) charCodes = charCodes.concat(number_char_code)
             if (includeSymbol) charCodes = charCodes.concat(symbol_char_code)
-            
+            //loop that pushes assigned characters
+
             let passwordCharacters = []
             for (let i = 0; i < characterAmount; i++) {
                 let characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
@@ -49,7 +51,7 @@ form.addEventListener("submit", e => {
             }
             return passwordCharacters.join('')
         }
-        
+        //creates random characters and assigns them to an array
         function arrayLowHigh(low, high) {
             let array = []
             for (let i = low; i <= high; i++) {
@@ -57,13 +59,13 @@ form.addEventListener("submit", e => {
             }
             return array
         }
-        
+        //determines character count
         function syncCharacterAmount(e) {
             let value = e.target.value
             characterAmountNumber.value = value
             characterAmountRange.value = value
         }
-
+        //function for copy button
         function myFunction() {
             /* Get the text field */
             var copyText = document.getElementById("display");
