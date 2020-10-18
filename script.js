5484b6c272435b28da4b9695fb074b08fa83a621
rich wilk <richardwilkinson@live.com>
2020-10-17 23:00:52 -0400

    accidentally saved over my hw in class with cw and had to restart.

--- script.js
+++ script.js
@@ -1,64 +1,18 @@
-//Generator functions
-
-function getRandomLow() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-console.log(getRandomLow());
-
-function getRandomUp() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-console.log(getRandomUp());
-
-function getRandomNum() {
-    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-console.log(getRandomNum());
-
-function getRandomSym() {
-    const symbols = "!@#$%^&*()_+";
-    return symbols[Math.floor(Math.random() * symbols.length)];
-}
-console.log(getRandomSym());
-//generate random password
+//Generate Random Password
 function generate(){
     //set password length/diversity
     let complexity = document.getElementById("slider").value;
-    console.log("complexity")
-    //possible password characters
-    let values = "ABCDEFGHIJKLMNOPQRSTUVWYXZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*()_+";
-    console.log("values")
+
+    //possible values
+    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*";
+
     let password = "";
 
-    //for loop to choose password character
+    //create loop for password
     for(var i = 0; i <= complexity; i++){
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
     }
 
-    //displays password in textbox
     document.getElementById("display").value = password;
-
-    //add password to previously generated section
-    document.getElementById("lastPass").innerHTML += password + "<br />";
-
 }
 
-//set default length display of 68
-document.getElementById("length").innerHTML = "length: 68"
-
-//function to set length in slider
-document.getElementById("slider").oninput = function(){
-    if( document.getElementById("slider").value > 0){
-        document.getElementById("length").innerHTML = "length: " + document.getElementById("slider").value;
-    }
-    else{
-        document.getElementById("length").innerHTML = "length: 1"
-    }
-}
-
-//function to copy pass in clipboard
-function copyPass(){
-    document.getElementById("display").select();
-    document.execCommand("copy");
-    alert("Password copied!");
-}
