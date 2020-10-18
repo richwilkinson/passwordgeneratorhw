74ba4ede099ef26246d6927270a10596f81071a6
rich wilk <richardwilkinson@live.com>
2020-10-18 04:45:01 -0400

    fixed js file to make some of my functions work, re coded html to fit new ids and changed some style as i moved away from bootstrap.

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
-//possible values
-let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let lowerCase = "abcdefghijklmnopqrstuvwxyz";
-let numbers = "123456789";
-let symbols = "!@#$%^&*";
-
-//Generate Random Password
+//generate random password
 function generate(){
-    //set password length/diversity
-    let complexity = document.getElementById("slider").value;
+    //set password length/complexity
+    const complexity = document.getElementById("slider").value;
+
+    //set possible password values
+    const values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*";
 
     let password = "";
-    
 
-
-    //create loop for password
-    for(var i = 0; i <= complexity; i++){
-        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
+    //create for loop to choose password
+    for(var i=0; i <= complexity; i++) {
+        password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length - 1)));
     }
 
-    //add password to textbook/display area
+    //add password to display
     document.getElementById("display").value = password;
 
-    //add password to previously created 
-    document.getElementById("lastPass").innerHTML = password + "<br />"
-
+    //add password to created section
+    document.getElementById("lastPass").innerHTML += password + "<br />";
 }
+
+//set default length display of 8
+document.getElementById("length").innerHTML = "length: 8";
+
+//set length
+document.getElementById("slider").oninput = function(){
+    if (document.getElementById("slider").value > 0){
+        document.getElementById("length").innerHTML = "length: " + document.getElementById("slider").value;
+    } else{
+        document.getElementById("length").innerHTML = "length: 1";
+    }
+}
+
+//function to copy password
+function copypassword(){
+    document.getElementById("display").select();
+
+    document.execCommand("Copy");
+
+    alert("password copied!")
+}
+
+
 
