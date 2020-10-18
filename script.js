7ad38c3d21776aed3b5489cbc9cb78d1c4f5a03c
rich wilk <richardwilkinson@live.com>
2020-10-17 23:42:06 -0400

    added var to js

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
+//possible values
+let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+let numbers = "123456789";
+let symbols = "!@#$%^&*";
+
 //Generate Random Password
 function generate(){
     //set password length/diversity
     let complexity = document.getElementById("slider").value;
 
-    //possible values
-    let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789!@#$%^&*";
-
     let password = "";
+    
+
 
     //create loop for password
     for(var i = 0; i <= complexity; i++){
         password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length -1)));
     }
 
+    //add password to textbook/display area
     document.getElementById("display").value = password;
+
+    //add password to previously created 
+    document.getElementById("lastPass").innerHTML = password + "<br />"
+
 }
 
