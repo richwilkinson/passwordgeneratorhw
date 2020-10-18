ffa52d5f6876651aa8e22cea03423846e5184fb2
rich wilk <richardwilkinson@live.com>
2020-10-18 14:54:42 -0400

    copy button n function

--- script.js
+++ script.js
@@ -63,3 +63,17 @@ form.addEventListener("submit", e => {
             characterAmountNumber.value = value
             characterAmountRange.value = value
         }
+
+        function myFunction() {
+            /* Get the text field */
+            var copyText = document.getElementById("display");
+          
+            /* Select the text field */
+            copyText.select();
+          
+            /* Copy the text inside the text field */
+            document.execCommand("copy");
+          
+            /* Alert the copied text */
+            alert("Copied the text: " + copyText.value);
+          }
